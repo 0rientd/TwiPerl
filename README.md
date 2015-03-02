6a672dcffa4f64b81d2b3e0e216ca400166a4f66
root <root@carlos-Elementary.(none)>
2015-03-01 22:04:11 -0300

    Release v1.0 of TwiPerl. A client Twitter in Perl

---
+++ README.md
@@ -0,0 +1,58 @@
+# TwiPerl v1.0
+--------------
+
+TwiPerl é um client de Twitter feito para linha de comando em linguagem Perl.
+
+Ele conta com 3 funções por enquanto, como:
+
+* Tuítar
+* Seguir pessoas colocando o nome de usuário ( *SEM ARROBA* )
+* Des-seguir pessoas colocando o nome de usuário ( *SEM ARROBA* )
+
+Em breve colocarei mais funções.
+--------------
+
+# MODO DE USO
+
+Para executar o script basta você caminhas até o diretório do TwiPerl no Terminal e digitar algum destes comandos:
+
+* perl TwiPerl.pl -tuitar
+* perl TwiPerl.pl -follow
+* perl TwiPerl.pl -unfollow
+--------------
+
+# DEPENDÊNCIAS
+
+warnings;
+strict;
+Net::Twitter;
+Data::Dumper;
+Scalar::Util 'blessed';
+WWW::Mechanize;
+Getopt::Long;
+utf8;
+Term::ANSIColor;
+
+Para instala-los você pode digitar "sudo cpan [NOME DO MÓDULO]
+PS: VOCÊ PRECISA USAR O SUDO POIS SE NÃO VAI DAR ERRO NA HORA DE INSTALAR OS MÓDULOS!
+--------------
+
+# COMO CONSEGUIR SUAS KEYS DE DESENVOLVEDOR
+
+* Você terá que estar logado na Twitter via Browser ( Chrome, Mozilla, Opera e etc... ) e ir nesse link -> https://apps.twitter.com/
+Este link é aonde você consegue suas keys para pode usar aplicações terceiras no Twitter.
+
+* Após isso você clicar no botão "Create New App".
+
+* Preencha os campos com o nome da aplicação, descrição, website ( pode colocar o seu Twitter ) e aceitar os termos e clicar no botão "Create your Twitter application".
+
+* Acesse a aba "Keys and Access Tokens" e você já verá a sua Consumer Key (API Ke), sua Consumer Secret (API Secret) e Access Level. Basta trocar de "Read-only" para "Read, Write and Access direct messages" e clicar no botão "Update Settings".
+
+* Feito isso, volte para a aba "Keys and Access Tokens" e clique no botão "Create my access token" quase que no final da página, para criação de mais chaves para acesso a sua conta.
+
+* Basta copia-las, "Consumer Key", "Consumer Secret", "Access Token", "Access Token Secret" e colar no código fonte do scipt aonde ele pede para você inserir suas Keys e salvar o script.
+
+* ATENÇÃO! NÃO PASSE ESSAS CHAVES PARA NINGUÉM POIS ELAS DÃO ACESSO AO SEU TWITTER!
+--------------
+
+Made
---
+++ TwiPerl.pl
@@ -0,0 +1,183 @@
+#!/usr/bin/perl 
+#
+# USE, EDITE, COMPILE. MAS SEMPRE DISTRIBUA O CÓDIGO FONTE!
+# CONHECIMENTO DEVE SER LIVRE E DE TODOS! 
+#
+# CODED BY: HackerOrientado
+#
+# Me sigam no twitter -> @HackerOrientado
+# Facebook -> facebook.com/CalsEvolution
+# Página Ciência Hacker -> facebook.com/CienciaHacker
+# Grupo Ciência Hacker -> facebook.com/groups/cienciahacker/
+# Página no Facebook do Inurl Brasil -> facebook.com/InurlBrasil
+# Blog Inurl Brasil -> blog.inurl.com.br/
+#
+# Obrigado ao GoogleINURL e ao @7mm5l pela força que me deram o conhecimento de como usar regex tanto nesse quanto no BypassCF
+# Link do BypassCF -> github.com/HackerOrientado/BypassCF
+#
+
+############################ MÓDULOS QUE ESTÃO SENDO USADOS E SERÃO USADOS FUTURAMENTE
+use warnings;
+use strict;
+use Net::Twitter;
+use Data::Dumper;
+use Scalar::Util 'blessed';
+use WWW::Mechanize;
+use Getopt::Long;
+use utf8;
+use Term::ANSIColor;
+
+############################ CODIFICAÇÃO utf8 NA ENTRADA E SAÍDA DO SCRIPT
+binmode(STDOUT, ":utf8");
+binmode(STDIN, ":utf8");
+
+############################ CRIAÇÃO DE VARIÁVEIS
+my ($baner1, $baner2, $SO, $limpar_tela, $url, $tuitar, $unfollow, $follow, $mech );
+$baner1 = "
+\t\t\t████████╗██╗    ██╗██╗
+\t\t\t╚══██╔══╝██║    ██║██║
+\t\t\t   ██║   ██║ █╗ ██║██║
+\t\t\t   ██║   ██║███╗██║██║
+\t\t\t   ██║   ╚███╔███╔╝██║
+\t\t\t   ╚═╝    ╚══╝╚══╝ ╚═╝";
+
+$baner2 = "
+\t\t\t██████╗ ███████╗██████╗ ██╗     
+\t\t\t██╔══██╗██╔════╝██╔══██╗██║     
+\t\t\t██████╔╝█████╗  ██████╔╝██║     
+\t\t\t██╔═══╝ ██╔══╝  ██╔══██╗██║     
+\t\t\t██║     ███████╗██║  ██║███████╗
+\t\t\t╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝
+\t\t\t                              v1.0\n\n";
+
+############################ VERIFICAÇÃO DO SISTEMA OPERACIONAL PARA LIMPEZA DE TELA
+$SO = $^O;
+if ( $SO eq "MSWin32" ) {
+    $limpar_tela = "cls";
+} else {
+    $limpar_tela = "clear";
+}
+
+system ("$limpar_tela");
+
+############################ KEYS DE DESENVOLVEDOR
+my $consumer_key = 'ENTRE COM SUA CHAVE DE DESENVOLVEDOR AQUI';
+my $consumer_secret = 'ENTRE COM SUA CHAVE DE DESENVOLVEDOR AQUI';
+my $access_token = 'ENTRE COM SUA CHAVE DE DESENVOLVEDOR AQUI';
+my $access_token_secret = 'ENTRE COM SUA CHAVE DE DESENVOLVEDOR AQUI';
+
+############################ INICIANDO AUTENTICAÇÃO COM O Net::Twitter
+my $nt = Net::Twitter->new (
+    traits              => ['API::RESTv1_1', 'OAuth'],
+    consumer_key        => $consumer_key,
+    consumer_secret     => $consumer_secret,
+    access_token        => $access_token,
+    access_token_secret => $access_token_secret,
+);
+ 
+############################ ARGUMENTOS
+my $args = GetOptions (
+    'tuitar'        => \$tuitar,
+    'unfollow'      => \$unfollow,
+    'follow'        => \$follow,
+);
+
+############################ CONDIÇÕES
+if ( $tuitar ) {
+
+    return tuitar();
+
+} elsif ( $unfollow ) {
+
+    return unfollow();
+
+} elsif ( $follow ) {
+    
+    return follow();
+    
+} else {
+    print color ('bold blue');
+    print $baner1;
+    print color ('reset');
+    print $baner2;
+
+    print "Digite o comando de ajuda para saber os argumentos que podem ser usados\n";
+    exit (0);
+
+}
+
+
+############################ SUB ROTINAS PARA SE USAR O CLIENTE
+sub tuitar () {
+    print color ('bold blue');
+    print $baner1;
+    print color ('reset');
+    print $baner2;
+
+    print "Digite uma tweet de ate 140 caracteres!\n";
+    print "-> ";
+    my $tuitar = <STDIN>; chomp $tuitar;
+
+    print "\n";
+    print "Publicando tweet...";
+
+    $nt -> update ({ status => "$tuitar" });
+
+    print "\n";
+    print "Tweet publicado com sucesso!\n\n";
+    print "Essa e sua mensagem -> $tuitar\n";
+    exit (0);
+}
+
+sub unfollow () {
+    print color ('bold blue');
+    print $baner1;
+    print color ('reset');
+    print $baner2;
+
+    print "Digite o ID do usuario a ser des-seguido\n";
+    print "--> ";
+    $unfollow = <STDIN>; chomp $unfollow;
+
+    $url = "http://gettwitterid.com/?user_name=".$unfollow."0&submit=GET+USER+ID";
+    $mech = WWW::Mechanize->new();
+
+    $mech->get ( $url );
+    $mech->content ( format => 'text' );
+
+        
+    if ( $mech =~ /Twitter User ID: (\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9})/ ) {
+        $unfollow = $1;
+    }
+
+    $nt->unfollow ({ id => "$unfollow" });
+
+    print "Twitter ID $unfollow foi des-seguido\n";
+    exit (0);    
+}
+
+sub follow () {
+    print color ('bold blue');
+    print $baner1;
+    print color ('reset');
+    print $baner2;
+
+    print "Digite o usuario do usuario a ser seguido\n";
+    print "--> ";
+    $follow = <STDIN>; chomp $follow;
+    
+    $url = "http://gettwitterid.com/?user_name=".$follow."0&submit=GET+USER+ID";
+    $mech = WWW::Mechanize->new();
+
+    $mech->get ( $url );
+    $mech->content ( format => 'text' );
+    
+    if ( $mech =~ /Twitter User ID: (\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9}\d{1,9})/ ) {
+        $follow = $1;
+    }
+
+    $nt->follow ({ id => "$follow" });
+    
+    print "Twitter ID $follow foi seguido\n";
+    exit (0);
+}
